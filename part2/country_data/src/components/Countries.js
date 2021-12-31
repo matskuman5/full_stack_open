e0e00eb1cd09750b7ac13114f7a9f3b3dca3fe70
Matias <selinmatias5@gmail.com>
2021-12-31 20:32:51 +0200

    only show results if filters max 10 countries

--- part2/country_data/src/components/Countries.js
+++ part2/country_data/src/components/Countries.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const Countries = ({countries}) => {
+
+    if (countries.length > 10) {
+        return (
+            <div>
+                Too many matches, use another filter
+            </div>
+        )
+    }
+
     return (
         <div>
             {countries.map(c => 
