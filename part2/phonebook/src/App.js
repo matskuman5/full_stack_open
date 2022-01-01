b663c02b57decb63f007bb282333430a77c8b6e7
Matias <selinmatias5@gmail.com>
2022-01-01 21:31:19 +0200

    use axios.post to sync numbers with server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -50,7 +50,14 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-    setPersons(persons.concat(personObject))
+
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        console.log(response)
+        setPersons(persons.concat(personObject))
+    })
+
     setNewName('')
     setNewNumber('')
   }
