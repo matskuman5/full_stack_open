4b3ba95ec08362156c3e2e4ce09a1e06fe4803d7
Matias <selinmatias5@gmail.com>
2022-06-21 23:41:00 +0300

    separate environment variable handling to config.js

--- part4/app.js
+++ part4/app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const password = process.argv[2]
+const config = require('./utils/config')
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -14,8 +13,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = `mongodb+srv://fullstackopen:${password}@cluster0.gkc9j.mongodb.net/blog_list?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
--- part4/index.js
+++ part4/index.js
@@ -1,10 +1,10 @@
 const http = require('http')
 const app = require('./app')
 const logger = require('./utils/logger')
+const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-const PORT = 3003
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
