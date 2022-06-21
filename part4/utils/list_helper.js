9abb7b1e5d0bc17d906bd4d7276fdbf1fba47cbe
Matias <selinmatias5@gmail.com>
2022-06-22 01:13:26 +0300

    add favoriteBlog function

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length
+    ? blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+    : []
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
