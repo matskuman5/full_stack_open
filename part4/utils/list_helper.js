f8dd07af0480f318fd67ea14b6a2e7a70ecf0005
Matias <selinmatias5@gmail.com>
2022-06-22 00:53:48 +0300

    add example list of blogs for testing

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  })
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
