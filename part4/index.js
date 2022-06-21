0d5de98170b5b9b1f6597103d28b3e69405ae5c7
Matias <selinmatias5@gmail.com>
2022-06-21 23:31:19 +0300

    separate console printing into logger.js

--- part4/index.js
+++ part4/index.js
@@ -1,9 +1,10 @@
 const http = require('http')
 const app = require('./app')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
 const PORT = 3003
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
