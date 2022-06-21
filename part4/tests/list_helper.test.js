80093638acb66fad47d8d016e11bb9497000092c
Matias <selinmatias5@gmail.com>
2022-06-22 01:19:21 +0300

    add one-blog test for favoriteBlog

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -51,6 +51,17 @@ const blogs = [
   }
 ]
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
 test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
@@ -62,17 +73,6 @@ describe('total likes', () => {
     const result = listHelper.totalLikes([])
     expect(result).toBe(0)
   })
-
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
 
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
@@ -91,6 +91,11 @@ describe('favorite blog', () => {
   test('of empty list is empty object', () => {
     const result = listHelper.favoriteBlog([])
     expect (result).toEqual({})
+  })
+
+  test('when list has only one blog equals that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
   })
 
 })
