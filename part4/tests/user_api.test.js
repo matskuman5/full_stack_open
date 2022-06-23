843024ce6bf302196d8ad3c184f77bd8e875dd96
Matias <selinmatias5@gmail.com>
2022-06-23 21:17:01 +0300

    add test for successfully adding a user

--- part4/tests/user_api.test.js
+++ part4/tests/user_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const User = require('../models/user')
+const testHelper = require('./test_helper')
 
 describe('get', () => {
 
@@ -12,4 +13,33 @@ describe('get', () => {
       .expect('Content-Type', /application\/json/)
   })
 
+})
+
+describe('post', () => {
+
+  beforeAll(async () => {
+    await User.deleteMany()
+  })
+
+  test('successfully adding user works correctly', async () => {
+    await api
+      .post('/api/users')
+      .send({
+        username: 'test',
+        name: 'john doe',
+        password: 'secret'
+      })
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/users')
+    console.log(response.body)
+    expect(response.body).toHaveLength(1)
+
+  })
+
+})
+
+afterAll(() => {
+  mongoose.connection.close()
 })
