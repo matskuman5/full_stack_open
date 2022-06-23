cff99612563a7706e63a429da6e884274692b5b7
Matias <selinmatias5@gmail.com>
2022-06-23 18:04:25 +0300

    fix typo in blogsRouter name

--- part4/app.js
+++ part4/app.js
@@ -5,13 +5,13 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const notesRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/blogs', notesRouter)
+app.use('/api/blogs', blogsRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
