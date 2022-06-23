c4ca6bcc8c5e84bd3a3dea85999ecd7816b389cd
Matias <selinmatias5@gmail.com>
2022-06-23 20:48:01 +0300

    add usersRouter and implement creating users

--- part4/app.js
+++ part4/app.js
@@ -6,12 +6,14 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
