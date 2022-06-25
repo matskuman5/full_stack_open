d7ffc15c5c0de4128382246c51298df18b25bd13
Matias <selinmatias5@gmail.com>
2022-06-25 23:53:23 +0300

    add clicking on blog titles to show more info

--- part5/frontend/src/components/Blog.js
+++ part5/frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <div>
+      {expanded
+        ? <div>
+            <button onClick={toggleExpanded}>{blog.title}</button>
+            <div>
+              <p>
+                {blog.url}
+              </p>
+              <p>
+                {blog.likes}
+                <button>like</button>
+              </p>
+              <p>
+                {blog.author}
+              </p>
+            </div>
+          </div>
+        : <div>
+            <button onClick={toggleExpanded}>{blog.title}</button>
+          </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
