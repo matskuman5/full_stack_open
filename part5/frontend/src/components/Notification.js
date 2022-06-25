90906a8f1531517567e96e0c5b6796a58b4244b8
Matias <selinmatias5@gmail.com>
2022-06-25 20:39:28 +0300

    make notifications bigger and color them accordingly

--- part5/frontend/src/components/Notification.js
+++ part5/frontend/src/components/Notification.js
@@ -1,11 +1,22 @@
 const Notification = ({ message }) => {
+
     if (message === null) {
       return null
     }
+
+    const notificationStyle = message.includes('error:')
+    ? {
+        color: 'red'
+    }
+    : {
+        color: 'green'
+    }
   
     return (
-      <div className="error">
-        {message}
+      <div className="error" style={notificationStyle}>
+        <h1>
+            {message}
+        </h1>
       </div>
     )
   }
