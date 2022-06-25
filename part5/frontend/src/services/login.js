69b398e2d3d58d3c85ab7ff61f6a4427aed271d4
Matias <selinmatias5@gmail.com>
2022-06-25 19:33:10 +0300

    add loginService service

---
+++ part5/frontend/src/services/login.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+const login = async credentials => {
+  const response = await axios.post(baseUrl, credentials)
+  return response.data
+}
+
+export default { login }
