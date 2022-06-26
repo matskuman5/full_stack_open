3c64b5852e2fc3afe6de2d53de20bb3127fdd03d
Matias <selinmatias5@gmail.com>
2022-06-26 22:07:08 +0300

    fix testing router formatting

--- part5/backend/controllers/testing.js
+++ part5/backend/controllers/testing.js
@@ -2,7 +2,7 @@ const testingRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-testingRouter.post('/reset', async(req, res) {
+testingRouter.post('/reset', async(req, res) => {
   await Blog.deleteMany()
   await User.deleteMany()
   res.status(204).end()
