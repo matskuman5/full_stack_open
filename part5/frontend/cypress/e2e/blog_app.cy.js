f7558617fb98ba4dcfaa09b1ba913065106a106e
Matias <selinmatias5@gmail.com>
2022-06-26 22:06:56 +0300

    add test to show login form when first arriving at site

--- part5/frontend/cypress/e2e/blog_app.cy.js
+++ part5/frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,11 @@
 describe('Blog app', function() {
-  it('test', function() {
+
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
+  })
+
+  it('login form is shown', function() {
+    cy.contains('Login')
   })
 })
