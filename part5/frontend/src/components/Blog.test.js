4fc09da1f62db3fa993119fa6066574bc3c0005f
Matias <selinmatias5@gmail.com>
2022-06-26 17:49:09 +0300

    wrap blog test in describe block

--- part5/frontend/src/components/Blog.test.js
+++ part5/frontend/src/components/Blog.test.js
@@ -3,17 +3,26 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 
-test('by default shows only title', () => {
-  const blog = {
-    title: 'test blog',
-    author: 'test person',
-    url: 'http something',
-    likes: 5
-  }
+describe('visibility', () => {
 
-  const { container } = render(<Blog blog={blog} />)
+  let container
 
-  screen.getByText('test blog')
-  expect(container).not.toHaveTextContent('test person')
+  beforeEach(() => {
+    const blog = {
+      title: 'test blog',
+      author: 'test person',
+      url: 'http something',
+      likes: 5
+    }
 
+    container = render(<Blog blog={blog} />).container
+  })
+
+  test('by default shows only title', () => {
+
+    screen.getByText('test blog')
+    expect(container).not.toHaveTextContent('test person')
+
+  })
 })
+
