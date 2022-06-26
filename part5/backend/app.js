03ca35d83d04fad959d8e19219150e76d1d5e9b2
Matias <selinmatias5@gmail.com>
2022-06-26 22:02:02 +0300

    add testing router to backend for resetting

--- part5/backend/app.js
+++ part5/backend/app.js
@@ -13,6 +13,10 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
