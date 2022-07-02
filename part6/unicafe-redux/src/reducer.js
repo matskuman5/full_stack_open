10e1549956dae68a1e18b41536f815edf0a8155c
Matias <selinmatias5@gmail.com>
2022-07-02 16:45:14 +0300

    add functionality and tests to 'ok' and 'bad' commands

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -8,15 +8,23 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      const newState = {
+      const goodIncremented = {
         ...state,
         good: state.good + 1
       }
-      return newState
+      return goodIncremented
     case 'OK':
-      return state
+      const okIncremented = {
+        ...state,
+        ok: state.ok + 1
+      }
+      return okIncremented
     case 'BAD':
-      return state
+      const badIncremented = {
+        ...state,
+        bad: state.bad + 1
+      }
+      return badIncremented
     case 'ZERO':
       return initialState
     default: return state
