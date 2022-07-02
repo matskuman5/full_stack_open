adf637452a72629f0b28beaf0a276cedf67b92f7
Matias <selinmatias5@gmail.com>
2022-07-03 00:58:23 +0300

    separate redux store config into store.js

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 
 const AnecdoteList = () => {
 
-    const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+    const anecdotes = useSelector(state => state.anecdotes.sort((a, b) => b.votes - a.votes))
     const dispatch = useDispatch()
     
     const vote = (id) => {
