eda8e87311a2eb6089665cbdc472b78d44e3014a
Matias <selinmatias5@gmail.com>
2022-07-04 01:41:06 +0300

    initialize anecdotes with an async action

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,14 +4,13 @@ import Notification from './components/Notification'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes, setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
 
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService.getAll()
-      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
