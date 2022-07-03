b51fb22b739d83e4280952c7080c1313515427a7
Matias <selinmatias5@gmail.com>
2022-07-04 01:12:27 +0300

    get initial anecdotes from backend

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,8 +1,19 @@
 import AnecdoteForm from './components/AnecdoteList'
 import AnecdoteList from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <Notification/>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,26 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-  {
-    content: 'If it hurts, do it more often',
-    id: '123456',
-    votes: 0
-  },
-  {
-    content: 'Adding manpower to a late software project makes it later!',
-    id: '100000',
-    votes: 1
-  },
-  {
-    content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    id: '111111',
-    votes: 3
-  }
-]
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState: anecdotesAtStart,
+  initialState: [],
   reducers: {
     addVote(state, action) {
       const id = action.payload
