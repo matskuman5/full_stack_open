0d832cca2665ee5373be467ef96b93de6990424a
Matias <selinmatias5@gmail.com>
2022-07-13 17:13:24 +0300

    remove unnecessary eslint rules

--- part7/part5/frontend/.eslintrc.js
+++ part7/part5/frontend/.eslintrc.js
@@ -21,30 +21,7 @@ module.exports = {
     'react', 'jest', 'cypress'
   ],
   'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
     'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
     'no-console': 0,
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 'off'
