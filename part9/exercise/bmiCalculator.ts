bb52cfbd2a0253e4688c7c9ed036452436446dc6
Matias <selinmatias5@gmail.com>
2023-01-07 14:37:16 +0200

    return string with description of bmi

--- part9/exercise/bmiCalculator.ts
+++ part9/exercise/bmiCalculator.ts
@@ -1,5 +1,14 @@
 const calculateBmi = (height: number, weight: number) => {
-  return weight / (height / 100) ** 2;
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi <= 18.4) {
+    return 'Underweight';
+  } else if (bmi > 18.4 && bmi <= 24.9) {
+    return 'Normal (healthy weight)';
+  } else if (bmi > 24.9 && bmi <= 29.9) {
+    return 'Overweight';
+  } else {
+    return 'Obese';
+  }
 };
 
 console.log(calculateBmi(180, 74));
