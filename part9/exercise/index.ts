99d30cdab4f2e36aaf89d1578ee9a2f2925cb9e5
Matias <selinmatias5@gmail.com>
2023-01-07 20:36:54 +0200

    add bmi endpoint

--- part9/exercise/index.ts
+++ part9/exercise/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  if (isNaN(Number(req.query.height)) || isNaN(Number(req.query.weight))) {
+    res.status(400).json({
+      error: 'malformatted parameters',
+    });
+    return;
+  }
+  const bmi = calculateBmi(Number(req.query.height), Number(req.query.weight));
+  res.json({
+    weight: req.query.weight,
+    height: req.query.height,
+    bmi: bmi,
+  });
 });
 
 const PORT = 3001;
