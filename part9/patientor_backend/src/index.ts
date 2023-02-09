d4968004eab89986f970e5d550309963567cfa63
Matias <selinmatias5@gmail.com>
2023-02-09 23:07:14 +0200

    add placeholder router for getting diagnoses

--- part9/patientor_backend/src/index.ts
+++ part9/patientor_backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnosesRouter';
 import cors from 'cors';
 const app = express();
 app.use(express.json());
@@ -11,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
