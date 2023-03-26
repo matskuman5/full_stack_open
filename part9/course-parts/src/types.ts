3d8d4d78259c2ff0ee49478c095ebe27ddeada0b
Matias <selinmatias5@gmail.com>
2023-03-26 20:56:52 +0300

    import types from instructions

--- part9/course-parts/src/types.ts
+++ part9/course-parts/src/types.ts
@@ -1,10 +1,35 @@
-interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: 'basic';
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: 'group';
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: 'background';
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
 
 interface CoursePartProps {
   courseParts: CoursePart[];
 }
 
-export type { CoursePart, CoursePartProps };
+export type {
+  CoursePartBase,
+  CoursePartBasic,
+  CoursePartGroup,
+  CoursePartBackground,
+  CoursePart,
+  CoursePartProps,
+};
