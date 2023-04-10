3a363fda7a79335bd07ddabc624f42e0e7aed552
Matias <selinmatias5@gmail.com>
2023-04-10 19:47:05 +0300

    error handling when posting new diaries

--- part9/flight_diaries/src/App.tsx
+++ part9/flight_diaries/src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
   const [newEntryVisibility, setNewEntryVisibility] = useState('');
   const [newEntryWeather, setNewEntryWeather] = useState('');
   const [newEntryComment, setNewEntryComment] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:3001/api/diaries').then((res) => {
@@ -26,12 +27,23 @@ const App = () => {
       comment: newEntryComment,
     };
     console.log(newEntry);
-    axios.post('http://localhost:3001/api/diaries', newEntry);
+    axios.post('http://localhost:3001/api/diaries', newEntry).catch((error) => {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          setErrorMessage(error.response.data);
+        }
+        console.error(error);
+        console.log('poop');
+      } else {
+        console.error(error);
+      }
+    });
   };
 
   return (
     <div>
       <div>
+        <h1 color="red">{errorMessage}</h1>
         <h2>create new entry</h2>
         <form onSubmit={createNewEntry}>
           <input
