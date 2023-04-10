9e22100d670d64566129785d839aa8acd495a112
Matias <selinmatias5@gmail.com>
2023-04-10 19:18:36 +0300

    get diaries and show them in frontend

--- part9/flight_diaries/src/App.tsx
+++ part9/flight_diaries/src/App.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import { DiaryEntry } from './types';
+import axios from 'axios';
+
 const App = () => {
-  return <div>hello world</div>;
+  const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/diaries').then((res) => {
+      console.log(res.data);
+      setDiaryEntries(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {diaryEntries.map((d) => (
+        <div>
+          <h1>{d.date}</h1>
+          <p>visibility: {d.visibility}</p>
+          <p>weather: {d.weather}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
+
